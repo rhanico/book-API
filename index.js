5a626436f84094e143b7a94321c57d77009d41b3
FRANZ RHANICO <109740904+rhanico@users.noreply.github.com>
2023-05-10 20:45:46 -0700

    added route

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
+app.use('/books', require('./controllers/books'))
+
 app.get('/', (req, res) => {
     res.send('Hello world!')
 })
